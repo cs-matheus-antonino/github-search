c5def5e1d0bc79defaeeaddc0a1915e56b344222
Matheus Antonino <matheus.t.antonino@accenture.com>
2020-05-08 16:50:57 -0300

    Create function for getRepos

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Repository } from 'src/app/models/repository.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  private _getReposByUserName(userName: string): Observable<Repository[]> {
+    return this.http.get<Repository[]>(
+      `${environment.API_URL}/${userName}/repos`
+    );
+  }
 }
