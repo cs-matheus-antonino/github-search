18b31dbffc47de8fe3f845164e1d21dcf34a1665
Matheus Antonino <matheus.t.antonino@accenture.com>
2020-05-08 17:20:39 -0300

    Create function get user

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -3,12 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Repository } from 'src/app/models/repository.model';
+import { User } from 'src/app/models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   constructor(private http: HttpClient) {}
+
+  private _getUserByUserName(userName: string): Observable<User> {
+    return this.http.get<User>(`${environment.API_URL}/${userName}`);
+  }
 
   private _getReposByUserName(userName: string): Observable<Repository[]> {
     return this.http.get<Repository[]>(
