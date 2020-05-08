e123d17c7cfb33e108ff7ec7824def8b6200a351
Matheus Antonino <matheus.t.antonino@accenture.com>
2020-05-08 16:50:04 -0300

    Add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -8,17 +10,14 @@ import { SearchComponent } from './components/search/search.component';
 import { ResultComponent } from './pages/result/result.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    SearchComponent,
-    ResultComponent
-  ],
+  declarations: [AppComponent, HomeComponent, SearchComponent, ResultComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
