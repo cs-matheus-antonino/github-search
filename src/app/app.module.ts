499883132dfe79df01475d9207fa74ebdae5d982
Matheus Antonino <matheus.t.antonino@accenture.com>
2020-05-08 18:09:11 -0300

    Add httpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
@@ -16,6 +17,7 @@ import { ResultComponent } from './pages/result/result.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSnackBarModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
