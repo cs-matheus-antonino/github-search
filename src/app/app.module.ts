94c322b602b1e51a8fdcc4e4fe751cf182c01c37
Matheus Antonino <matheus.t.antonino@accenture.com>
2020-05-08 13:40:26 -0300

    Create component result

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SearchComponent } from './components/search/search.component';
+import { ResultComponent } from './pages/result/result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SearchComponent
+    SearchComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
