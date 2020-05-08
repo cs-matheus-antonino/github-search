abf635298f4d0574158498dd056dc7b030f8572a
Matheus Antonino <matheus.t.antonino@accenture.com>
2020-05-08 20:03:03 -0300

    Add formBuilder

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
+  form;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService
+  ) {
+    this.form = this.formBuilder.group({
+      userName: '',
+    });
   }
 
+  ngOnInit(): void {}
+
+  onSubmit() {
+    this.userService.searchUserByUserName(this.form.value.userName);
+  }
 }
