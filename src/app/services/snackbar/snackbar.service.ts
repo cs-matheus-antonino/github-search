38449025ecbdd601f01638add04ee3365ffe28e5
Matheus Antonino <matheus.t.antonino@accenture.com>
2020-05-08 17:18:17 -0300

    Create snackbar service

---
+++ src/app/services/snackbar/snackbar.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { EMPTY, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SnackbarService {
+  constructor(private snackBar: MatSnackBar) {}
+
+  showMessageError(msg: string): void {
+    this.snackBar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      panelClass: ['msg-error'],
+    });
+  }
+}
