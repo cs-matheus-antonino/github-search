6c18b8e1c03ba7e5afbd34ad58cd37bdcc95a4a1
Matheus Antonino <matheus.t.antonino@accenture.com>
2020-05-15 16:00:24 -0300

    Get query params and search user

--- src/app/pages/result/result.component.ts
+++ src/app/pages/result/result.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
-  styleUrls: ['./result.component.scss']
+  styleUrls: ['./result.component.scss'],
 })
 export class ResultComponent implements OnInit {
+  user: User;
+  userName: string;
+  loading = false;
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.loading = true;
 
+    this.activatedRoute.queryParams.subscribe((params) => {
+      this.userService.searchUserByUserName(params.userName);
+    });
+
+    this.userService.getUser().subscribe((user) => {
+      if (user) {
+        this.user = user;
+        this.loading = false;
+      }
+    });
+  }
 }
