0fdd01eb4d7f98d93500e21c27198c2dd591c208
Matheus Antonino <matheus.t.antonino@accenture.com>
2020-05-18 11:32:59 -0300

    Adjust layout

--- src/app/pages/result/result.component.ts
+++ src/app/pages/result/result.component.ts
@@ -22,11 +22,9 @@ export class ResultComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadingService.getLoading().subscribe((flag) => (this.loading = flag));
-
     this.activatedRoute.queryParams.subscribe((params) => {
       this.userService.searchUserByUserName(params.userName);
     });
-
     this.userService.getUser().subscribe((user) => {
       if (user) {
         this.user = user;
