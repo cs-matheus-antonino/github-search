ef26c3bd2638d2b69c81707bf658fe637ae4316d
Matheus Antonino <matheus.t.antonino@accenture.com>
2020-05-18 10:09:44 -0300

    Implement loading services

--- src/app/services/loading/loading.service.ts
+++ src/app/services/loading/loading.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoadingService {
+  private loading = new BehaviorSubject<boolean>(false);
 
-  constructor() { }
+  constructor() {}
+
+  getLoading(): Observable<boolean> {
+    return this.loading.asObservable();
+  }
+
+  showLoading(): void {
+    this.loading.next(true);
+  }
+
+  hideLoading(): void {
+    this.loading.next(false);
+  }
 }
